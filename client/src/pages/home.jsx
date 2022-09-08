abb372d500125e7e36c44055465bd933c0bdd22c
unknown <swarajkumarchaudhary1729@gmail.com>
2022-09-08 22:27:29 +0530

    [ADD] client/ data.js, categories.jsx [MOD] home.jsx
    Home page featured with various categories of products

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Introduction from '../partials/introduction'
 import Footer from '../partials/footer'
+import Categories from '../partials/categories'
 
 const Home = () => {
     return (
         <div>
             <Introduction />
+            <Categories />
             <Footer />
         </div>
     )
---
+++ client/src/partials/categories.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+import categories from '../data'
+
+const Container = styled.div`
+    width: 100%;
+    padding: 20px;
+	display: flex;
+	justify-content: space-around;
+`
+
+const Card = styled.div`
+    flex: 1;
+    height: 80vh;
+    margin: 10px;
+    position: relative;
+`
+const CategoryImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+const Info = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+const Title = styled.h1`
+    color: white;
+`
+const Button = styled.button`
+    padding: 10px 20px;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+`
+
+const Categories = () => {
+    return (
+        <Container>
+            {
+                categories.map(item => {
+                    return (
+                        <Card key={item.id}>
+                            <CategoryImage src={item.image} />
+                            <Info>
+                                <Title>{item.title}</Title>
+                                <Button>SHOP</Button>
+                            </Info>
+                        </Card>
+                    )
+                })
+            }
+        </Container>
+    )
+}
+
+export default Categories
