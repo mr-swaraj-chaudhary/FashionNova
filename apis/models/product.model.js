23c3aaf2a4b4cdba9cbf47494a1a83ee6b31756c
unknown <swarajkumarchaudhary1729@gmail.com>
2022-09-09 21:50:42 +0530

    [ADD] apis/ user.model.js,product.model.js

---
+++ apis/models/product.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    inStock: {
+        type: Boolean,
+        default: false
+    },
+    sizes: {
+        type: Array,
+        required: true
+    },
+    colors: {
+        type: Array,
+        required: true
+    },
+    categories: {
+        type: Array,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("product", productSchema)
