897bb81fb73674100b921b2f98a80daca969e55f
unknown <swarajkumarchaudhary1729@gmail.com>
2022-09-12 22:43:37 +0530

    [MOD] admin/ request.js

--- admin/src/requests.js
+++ admin/src/requests.js
@@ -1,15 +1,16 @@
 import axios from 'axios'
-const BASE_URL = "http://localhost:5000/apis"
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMWI4ODY1NjgyMTNkYzM3MTIyZGE5NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2Mjk4OTE3OCwiZXhwIjoxNjYzMjQ4Mzc4fQ.2SRjmv8Eji_L4TsGAbm7WJNgxhJ1w1hQ6iC9hXoNYq0"
+
+// admin token
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMWI4ODY1NjgyMTNkYzM3MTIyZGE5NiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzAwMjczOCwiZXhwIjoxNjYzMjYxOTM4fQ.FXODltfzhbvR6IBS6L6CtEqQbZxt9Hez7AqXVcNfgQE"
 
 // axios object for public resources
 const publicRequests = axios.create({
-    baseURL: BASE_URL
+    baseURL: "http://localhost:5000/apis"
 })
 
 // axios object for admin controlled resources
 const userRequests = axios.create({
-    baseURL: BASE_URL,
+    baseURL: "http://localhost:5000/apis",
     headers: {
         token: `Bearer ${TOKEN}`
     }
